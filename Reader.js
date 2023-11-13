f4cf6095ed7d2c33dfd7b2a91cd9b7115d24dc54
gutaq1 <gutaq1@gmail.com>
2023-11-13 15:17:07 -0300

    Promisify_aula_180

--- Reader.js
+++ Reader.js
@@ -2,22 +2,20 @@ const fs = require("fs");
 const util = require("util");
 
 
-
 class Reader {
 
     constructor() {
         this.reader = util.promisify(fs.readFile);
     }
 
-    Read(filepath) {
-        fs.readFile(filepath, "utf8", (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(data);
-            }
-        })
-    }
+    async Read(filepath) {
+        try{
+           return await this.reader(filepath, "utf8") 
+        }catch(err){
+            return undefined;
+        }
+    }    
 }
+
 
 module.exports = Reader;
--- app.js
+++ app.js
@@ -2,4 +2,9 @@ var Reader = require("./Reader");
 
 var leitor = new Reader();
 
-leitor.Read("./users.csv");
+async function main(){
+    var dados = await leitor.Read("./users.csv");
+    //console.log(dados);
+}
+
+main();
